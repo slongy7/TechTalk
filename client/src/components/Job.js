d89d47b2b470678e1494783764f7e9dea3b900ec
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-10-13 21:32:07 -0400

    generating feed page

---
+++ client/src/components/Job.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Job = () => {
+
+    return (
+        <div className="card">
+        <div className="card-image">
+            <figure className="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+            </figure>
+        </div>
+    </div>
+    );
+}
+
+export default Job;
